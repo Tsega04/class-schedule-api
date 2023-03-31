365999905a7c885668b0b01d76e7e876b5e206ca
Tsega Assa <tsegaassa@gmail.com>
2023-03-31 06:41:36 +0300

    created springboot project

---
+++ src/main/java/com/act/classscheduleapi/ClassScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package com.act.classscheduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassScheduleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassScheduleApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/act/classscheduleapi/ClassScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.act.classscheduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassScheduleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
